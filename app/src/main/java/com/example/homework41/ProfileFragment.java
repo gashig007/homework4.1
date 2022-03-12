6fb48964745224e52f326acade43f7aae08260bd
gashig007 <rustamgashigulin2001@gmail.com>
2022-03-12 13:53:43 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework41/ProfileFragment.java
@@ -0,0 +1,53 @@
+package com.example.homework41;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+import com.example.homework41.databinding.FragmentProfileBinding;
+
+import java.nio.channels.GatheringByteChannel;
+
+public class ProfileFragment extends Fragment {
+    private FragmentProfileBinding binding;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(LayoutInflater.from(getContext()), container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri uri) {
+                        // Handle the returned Uri
+                        Glide.with(binding.image).load(uri).into(binding.image);
+                    }
+                });
+        binding.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGetContent.launch("image/*");
+            }
+        });
+
+    }
+}
